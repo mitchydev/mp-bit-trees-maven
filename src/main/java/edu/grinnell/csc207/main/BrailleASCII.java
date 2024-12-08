3c265e8b1e8457c247d9e1641c874fb1bfc9b764
mitchydev <paivamit@grinnell.edu>
2024-12-08 16:14:06 -0600

    Implement main class

--- src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
+++ src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
@@ -1,22 +1,39 @@
 package edu.grinnell.csc207.main;
 
-import java.io.PrintWriter;
+import edu.grinnell.csc207.util.BrailleAsciiTables;
 
-/**
- *
- */
 public class BrailleASCII {
-  // +------+--------------------------------------------------------
-  // | Main |
-  // +------+
+    public static void main (String[] args) {
+        if (args.length < 2) {
+            System.out.println("Wrong input length.");
+            return;
+        }
 
-  /**
-   *
-   */
-  public static void main(String[] args) {
-    PrintWriter pen = new PrintWriter(System.out, true);
-    // STUB
-    pen.close();
-  } // main(String[]
+        System.out.println("Input1: " + args[0]);
+        System.out.println("Input2: " + args[1]);
+        String input1 = args[0].toLowerCase();
+        String input2 = args[1];
 
-} // class BrailleASCII
+        try {
+            switch (input1) {
+            case "braille":
+                StringBuilder braille = new StringBuilder();
+                for (int i = 0; i < input2.length(); i++) {
+                    braille.append(BrailleAsciiTables.toBraille(input2.charAt(i)));
+                }
+                System.out.println(braille.toString());
+                break;
+            case "ascii":
+                System.out.println(BrailleAsciiTables.toAscii(input2));
+                break;
+            case "unicode":
+                System.out.println(BrailleAsciiTables.toUnicode(input2));
+                break;
+            default:
+                System.err.println("Invalid entry.");
+            }
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
